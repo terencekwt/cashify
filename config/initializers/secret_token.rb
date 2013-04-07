7f8b05a1b22177cf5782bfbd11959568fd406f36
Terence <terencetam@berkeley.edu>
2013-04-06 23:14:38 -0700

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cashify::Application.config.secret_token = '352c799f190c09e45a97fdb96cf883b93f881688e19b5fdf2c12c57116acfefda3aab08c364d5f0fe2de67236814134b7934808d375426cb7842241ab92890d5'
