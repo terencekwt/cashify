51f4ae8a41d2ef5df8c8a9a611b1a826a07dc2ca
Terence <terencetam@berkeley.edu>
2013-04-13 18:52:23 -0700

    sign in system fully working, can post message under username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130413110713) do
+ActiveRecord::Schema.define(:version => 20130414005318) do
 
   create_table "boards", :force => true do |t|
     t.string   "title",      :limit => 50
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(:version => 20130413110713) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
