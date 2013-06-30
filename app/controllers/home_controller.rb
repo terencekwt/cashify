7ef232bf3b27431b79876b5d0ef35cced7b1562a
Terence <terencetam@berkeley.edu>
2013-06-30 00:38:28 -0700

    added login page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+      if user_signed_in?
+          redirect_to '/home/dashboard'
+      else 
+          render :layout => 'login'
+      end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Cashify::Application.routes.draw do
   resources :users
 
   get "home/index"
+  get "home/dashboard"
 
   root :to=> "home#index"
 
